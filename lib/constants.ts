6f5cb43c6f19913aa1a119e733cedfee79578c05
Rip58 <sergibenamor@me.com>
2025-12-31 11:09:08 +0100

    chore: bump version to 1.3.6

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.3.5'
+export const APP_VERSION = '1.3.6'
 export const APP_NAME = 'Gestor de SAT'
 export const APP_DESCRIPTION = 'Sistema de Gestión de Servicio Técnico'
